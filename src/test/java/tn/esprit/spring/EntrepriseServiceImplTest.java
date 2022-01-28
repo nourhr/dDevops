e793a590702faa9cc7148bd38d1ff73c4a702f87
BenAmor1 <mohamed.benamor.1@esprit.tn>
2022-01-28 19:23:19 +0100

    last change for class test

--- src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 import tn.esprit.spring.entities.Entreprise;
 
 import tn.esprit.spring.services.IEntreprise;
@@ -20,49 +19,49 @@ import tn.esprit.spring.services.IEntreprise;
 public class EntrepriseServiceImplTest {
 	@Autowired
 	IEntreprise IE;
-	
+
 	@Test
 	@Order(1)
-	public void testRestrieveAllEntreprises(){
+	public void testRestrieveAllEntreprises() {
 		List<Entreprise> listEntreprises = IE.retrieveAllEntreprises();
-		Assertions.assertEquals(0, listEntreprises.size());	
+		Assertions.assertEquals(0, listEntreprises.size());
 	}
-	
+
 	@Test
 	@Order(2)
-	public void  testAddEntreprise() throws ParseException {
-		Entreprise ent = new Entreprise ("perolla","jhutge2");
+	public void testAddEntreprise() throws ParseException {
+		Entreprise ent = new Entreprise("perolla", "jhutge2");
 		Entreprise entAded = IE.addEntreprise(ent);
 		Assertions.assertEquals(ent.getName(), entAded.getName());
 	}
-	
+
 	@Test
 	@Order(3)
-	public void testRestrieveAllEntreprisesSecond(){
+	public void testRestrieveAllEntreprisesSecond() {
 		List<Entreprise> listEnts = IE.retrieveAllEntreprises();
 		Assertions.assertEquals(1, listEnts.size());
 	}
-	
+
 	@Test
 	@Order(4)
-	public void  testUpdateEntreprise() throws ParseException {
-		Entreprise e = new Entreprise (1L,"MB","1548dsf");
+	public void testUpdateEntreprise() throws ParseException {
+		Entreprise e = new Entreprise(1L, "MB", "MB");
 		Entreprise entAded = IE.addEntreprise(e);
 		Assertions.assertEquals(e.getName(), entAded.getStatutFiscal());
 	}
-	
+
 	@Test
 	@Order(5)
-	public void testRetrieveEntreprise(){
-		Entreprise EntRetrived = IE.retrieveEntreprise((long) 3);
+	public void testRetrieveEntreprise() {
+		Entreprise EntRetrived = IE.retrieveEntreprise((long) 1);
 		Assertions.assertEquals(1L, EntRetrived.getId());
 	}
-	
+
 	@Test
 	@Order(6)
-	public void testDeletEntreprise(){
-		IE.deleteEntreprise((long)2);
-	
+	public void testDeletEntreprise() {
+		IE.deleteEntreprise((long) 1);
+
 		List<Entreprise> listEntreprises = IE.retrieveAllEntreprises();
 		Assertions.assertEquals(0, listEntreprises.size());
 	}
