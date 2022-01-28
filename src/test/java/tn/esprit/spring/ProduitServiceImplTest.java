836212150728636cece07cb9b2a2f795f29e4199
mbenamor <mbenamor@DESKTOP-Beffa.3s.com.tn>
2022-01-28 01:06:46 +0100

    test classes

--- src/test/java/tn/esprit/spring/ProduitServiceImplTest.java
+++ src/test/java/tn/esprit/spring/ProduitServiceImplTest.java
@@ -68,7 +68,7 @@ public class ProduitServiceImplTest {
 	@Test
 	@Order(5)
 	public void testRetrieveProduct() {
-		int l = 3 + agent.retrieveAllProduit().size();
+		int l = agent.retrieveAllProduit().size();
 		Produit p = agent.retrieveProduit((long) l);
 		Assertions.assertEquals(l, p.getIdProduit());
 	}
@@ -76,7 +76,7 @@ public class ProduitServiceImplTest {
 	@Test
 	@Order(6)
 	public void testDeleteProduct() {
-		int l = 3 + agent.retrieveAllProduit().size();
+		int l =  agent.retrieveAllProduit().size();
 		agent.deleteProduit((long) l);
 		int i = agent.retrieveAllProduit().size() - 1;
 		List<Produit> listProduct = agent.retrieveAllProduit();
